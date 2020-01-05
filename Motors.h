f869808137c84e49fbaff565482283eee14b3b20
Navid Adelpour <navidadelpour@gmail.com>
2020-01-06 03:25:05 +0330

    improve motor to move backward if needed

--- Motors.h
+++ Motors.h
@@ -45,20 +45,51 @@ class Motors {
         digitalWrite(right_in2, LOW);
     }
 
+    void set_motor(char *motor, char *direction, int speed) {
+        if(motor == "left") {
+            if(direction == "forward") {
+                digitalWrite(left_in1, LOW);
+                digitalWrite(left_in2, HIGH);
+            } else if (direction == "backward") {
+                digitalWrite(left_in1, HIGH);
+                digitalWrite(left_in2, LOW);
+            }
+            analogWrite(left_en, speed);
+        } else if(motor == "right") {
+            if(direction == "forward") {
+                digitalWrite(right_in1, HIGH);
+                digitalWrite(right_in2, LOW);
+            } else if (direction == "backward") {
+                digitalWrite(right_in1, LOW);
+                digitalWrite(right_in2, HIGH);
+            }
+            analogWrite(right_en, speed);
+        }
+    }
+
     void drive(int speed_difference, bool debug) {
-        int left_speed = constrain(normal_speed - speed_difference, min_speed, max_speed);
-        int right_speed = constrain(normal_speed + speed_difference, min_speed, max_speed);
-    
+        int left_speed = normal_speed + speed_difference;
+        int right_speed = normal_speed - speed_difference;
+
+        if(!debug) {
+            if(left_speed > 0) {
+                set_motor("left", "forward", constrain(left_speed, min_speed, max_speed));
+            } else {
+                set_motor("left", "backward", constrain(-left_speed, min_speed, max_speed));
+            }
+
+            if(right_speed > 0) {
+                set_motor("right", "forward", constrain(right_speed, min_speed, max_speed));
+            } else {
+                set_motor("right", "backward", constrain(-right_speed, min_speed, max_speed));
+            }
+        }
+
         Serial.print("left_speed: ");
         Serial.println(left_speed);
 
         Serial.print("right_speed: ");
         Serial.println(right_speed);
-
-        if(!debug) {
-            analogWrite(left_en, normal_speed - speed_difference);
-            analogWrite(right_en, normal_speed + speed_difference);
-        }
     }
 
 };
