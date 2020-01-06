d7051f156f61dd7db5b828a687c0f570196d8f14
Navid Adelpour <navidadelpour@gmail.com>
2020-01-06 12:57:36 +0330

    use enum in motors

--- Motors.h
+++ Motors.h
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 
 class Motors {
     public:
@@ -14,6 +13,9 @@ class Motors {
     int right_in1;
     int right_in2;
     int right_en;
+
+    enum Direction {FORWARD, BACKWARD};
+    enum Motor {LEFT, RIGHT};
 
     Motors (int normal_speed, int min_speed, int max_speed, int left_in1, int left_in2, int left_en, int right_in1, int right_in2, int right_en) {
         this->normal_speed = normal_speed;
@@ -37,29 +39,23 @@ class Motors {
         pinMode(right_en, OUTPUT);
         pinMode(right_in1, OUTPUT);
         pinMode(right_in2, OUTPUT);
-
-        digitalWrite(left_in1, LOW);
-        digitalWrite(left_in2, HIGH);
-
-        digitalWrite(right_in1, HIGH);
-        digitalWrite(right_in2, LOW);
     }
 
-    void set_motor(char *motor, char *direction, int speed) {
-        if(motor == "left") {
-            if(direction == "forward") {
+    void set_motor(Motor motor, Direction direction, int speed) {
+        if(motor == LEFT) {
+            if(direction == FORWARD) {
                 digitalWrite(left_in1, LOW);
                 digitalWrite(left_in2, HIGH);
-            } else if (direction == "backward") {
+            } else if (direction == BACKWARD) {
                 digitalWrite(left_in1, HIGH);
                 digitalWrite(left_in2, LOW);
             }
             analogWrite(left_en, speed);
-        } else if(motor == "right") {
-            if(direction == "forward") {
+        } else if(motor == RIGHT) {
+            if(direction == FORWARD) {
                 digitalWrite(right_in1, HIGH);
                 digitalWrite(right_in2, LOW);
-            } else if (direction == "backward") {
+            } else if (direction == BACKWARD) {
                 digitalWrite(right_in1, LOW);
                 digitalWrite(right_in2, HIGH);
             }
@@ -67,29 +63,32 @@ class Motors {
         }
     }
 
+    Direction get_direction(int speed) {
+        return speed > 0 ? FORWARD : BACKWARD;
+    }
+
+    int normalize_speed(int speed) {
+        return constrain(abs(speed), min_speed, max_speed);
+    }
+
     void drive(int speed_difference, bool debug) {
         int left_speed = normal_speed + speed_difference;
         int right_speed = normal_speed - speed_difference;
 
         if(!debug) {
-            if(left_speed > 0) {
-                set_motor("left", "forward", constrain(left_speed, min_speed, max_speed));
-            } else {
-                set_motor("left", "backward", constrain(-left_speed, min_speed, max_speed));
-            }
-
-            if(right_speed > 0) {
-                set_motor("right", "forward", constrain(right_speed, min_speed, max_speed));
-            } else {
-                set_motor("right", "backward", constrain(-right_speed, min_speed, max_speed));
-            }
+            set_motor(LEFT, get_direction(left_speed), normalize_speed(left_speed));
+            set_motor(RIGHT, get_direction(right_speed), normalize_speed(right_speed));
         }
 
         Serial.print("left_speed: ");
-        Serial.println(left_speed);
+        Serial.print(normalize_speed(left_speed));
+        Serial.print(" - ");
+        Serial.println(get_direction(left_speed) == FORWARD ? "F" : "B");
 
         Serial.print("right_speed: ");
-        Serial.println(right_speed);
+        Serial.print(normalize_speed(right_speed));
+        Serial.print(" - ");
+        Serial.println(get_direction(right_speed) == FORWARD ? "F" : "B");
     }
 
 };
